8654252468e487f075a5b6a4f77e411d671e4976
Ernest Ng <eyn5@cornell.edu>
2025-09-02 11:23:47 -0400

    Comments for server-03

--- examples/server-03.rs
+++ examples/server-03.rs
@@ -1,20 +1,31 @@
 use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpListener};
 
+// Echo server that adds a heart emoji to the end of every message
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let server = TcpListener::bind("127.0.0.1:42069").await?;
+    
     loop {
         let (mut tcp, _) = server.accept().await?;
         let mut buffer = [0u8; 16];
+        
         loop {
             let n = tcp.read(&mut buffer).await?;
             if n == 0 {
                 break;
             }
+            
+            // Convert byte slice to a `String`
             let mut line = String::from_utf8(buffer[..n].to_vec())?;
+            
+            // Remove the line-terminating chars that were added by Telnet
             line.pop(); // remove \n char
             line.pop(); // remove \r char
+
+            // Add our own line terminator
             line.push_str(" ❤️\n");
+            
             let _ = tcp.write(line.as_bytes()).await?;
         }
     }

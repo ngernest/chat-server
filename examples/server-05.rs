56d3fac14e51926dd9c9172736623157cfcc2ea5
Ernest Ng <eyn5@cornell.edu>
2025-09-02 11:31:13 -0400

    Comments for server-05

--- examples/server-05.rs
+++ examples/server-05.rs
@@ -2,6 +2,7 @@ use futures::{SinkExt, StreamExt};
 use tokio::net::TcpListener;
 use tokio_util::codec::{FramedRead, FramedWrite, LinesCodec};
 
+// Include the contents of `help-01.txt` as a string
 const HELP_MSG: &str = include_str!("shared/help-01.txt");
 
 #[tokio::main]
@@ -10,15 +11,22 @@ async fn main() -> anyhow::Result<()> {
     loop {
         let (mut tcp, _) = server.accept().await?;
         let (reader, writer) = tcp.split();
+
         let mut stream = FramedRead::new(reader, LinesCodec::new());
         let mut sink = FramedWrite::new(writer, LinesCodec::new());
+        
+        // Send a list of server commands to the sink 
         sink.send(HELP_MSG).await?;
+        
         while let Some(Ok(mut msg)) = stream.next().await {
+            // Handle /help command
             if msg.starts_with("/help") {
                 sink.send(HELP_MSG).await?;
             } else if msg.starts_with("/quit") {
+                // Handle /quit command
                 break;
             } else {
+                // Handle regular messages
                 msg.push_str(" ❤️");
                 sink.send(msg).await?;
             }

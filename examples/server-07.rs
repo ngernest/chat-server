a6965eb33a9a78c1fd1300c92b9f72bd78cdcc29
Ernest Ng <eyn5@cornell.edu>
2025-09-06 12:03:51 -0400

    Comments for server-07

--- examples/server-07.rs
+++ examples/server-07.rs
@@ -7,9 +7,12 @@ const HELP_MSG: &str = include_str!("shared/help-01.txt");
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let server = TcpListener::bind("127.0.0.1:42069").await?;
+    // Create broadcast channel for communicating between different clients
+    // `tx` is the sender for the channel 
     let (tx, _) = broadcast::channel::<String>(32);
     loop {
         let (tcp, _) = server.accept().await?;
+        // For each connected client, we have to clone the broadcast channel
         tokio::spawn(handle_user(tcp, tx.clone()));
     }
 }
@@ -18,6 +21,8 @@ async fn handle_user(mut tcp: TcpStream, tx: Sender<String>) -> anyhow::Result<(
     let (reader, writer) = tcp.split();
     let mut stream = FramedRead::new(reader, LinesCodec::new());
     let mut sink = FramedWrite::new(writer, LinesCodec::new());
+    
+    // Get a receiver from the sender
     let mut rx = tx.subscribe();
     sink.send(HELP_MSG).await?;
     while let Some(Ok(mut user_msg)) = stream.next().await {
@@ -27,8 +32,11 @@ async fn handle_user(mut tcp: TcpStream, tx: Sender<String>) -> anyhow::Result<(
             break;
         } else {
             user_msg.push_str(" ❤️");
+            // Send all messages to the channel
             tx.send(user_msg)?;
         }
+         // receive all of our and others'
+        // messages from the channel
         let peer_msg = rx.recv().await?;
         sink.send(peer_msg).await?;
     }
